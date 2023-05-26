c7a69658a750f3e0847d1789bfeec9ce794ffaf6
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-05-26 17:51:14 +0200

    Add preprocessing run VEP for mouse and parse out

---
+++ build/preprocessing/parse_vep_out.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import argparse
+
+# Parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_vcf", help = "Path of the vcf file used as input", type=str, required=True)
+parser.add_argument("-o", "--output_path", help = "Path of the output file", type=str, required=True)
+args = parser.parse_args()
+input_vcf = args.input_vcf
+output_path = args.output_path
+
+# Load vcf and rename columns for the output file
+out_vep = pd.read_csv(input_vcf, sep='\t')
+vep_cols = ["#Uploaded_variation", "Feature", "Consequence", "SYMBOL", "CANONICAL", "Protein_position", "Amino_acids"]
+out_vep = out_vep[vep_cols]
+out_vep = out_vep.rename(columns={"#Uploaded_variation" : "Tumor_Sample_Barcode",
+                                  "Consequence" : "Variant_Classification",
+                                  "SYMBOL" : "Hugo_Symbol",
+                                  "Feature" : "Transcript_ID"})
+
+# Select only mutations mapped to canonical transcript 
+out_vep = out_vep[out_vep["CANONICAL"] == "YES"]
+
+# Parse mutation on the protein
+out_vep["HGVSp_Short"] =  out_vep.apply(lambda x: 
+                              "p." + x["Amino_acids"].split("/")[0] + str(x["Protein_position"]) + x["Amino_acids"].split("/")[1] 
+                              if len(x["Amino_acids"].split("/")) > 1 
+                              else np.nan, axis=1)
+out_vep = out_vep[["Hugo_Symbol", "Tumor_Sample_Barcode", "Variant_Classification", "Transcript_ID", "HGVSp_Short"]]
+out_vep.to_csv(output_path, sep="\t", index=False)
---
+++ build/preprocessing/vep_and_parse_mouse.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Help message
+function show_help() {
+    echo "Usage: ./vep_and_parse.sh [option1] [option2]"
+    echo ""
+    echo "Options:"
+    echo "  option1 : Specify the input vcf file."
+    echo "  option2 : Specify the output file (e.g. .maf)."
+    echo ""
+    echo "Example:"
+    echo "/workspace/projects/clustering_3d/clustering_3d/build/preprocessing/vep_and_parse_mouse.sh /input.vcf /output.maf"
+    echo ""
+}
+
+# Check if the help flag is provided
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    show_help
+    exit 0
+fi
+
+# Check if arguments are missing
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Error: Missing option1 or option2."
+    show_help
+    exit 0
+fi
+
+# Run VEP
+singularity exec /workspace/datasets/vep/mus_musculus/vep102.sif vep --dir /workspace/datasets/vep/ -i $1 --offline --cache -o $2 --species mus_musculus --assembly GRCm38 --fork 8 --symbol --protein --tab --canonical --pick
+# Remove VEP summary
+rm $2"_summary.html"
+
+## Remove comments in the header
+# Create a temporary file
+temp_file=$(mktemp)  
+# Filter the content and store it in the temporary file
+cat "$2" | grep -v "^##" > "$temp_file"
+# Overwrite the original file 
+mv "$temp_file" "$2"
+
+# Parse output
+python3 /workspace/projects/clustering_3d/clustering_3d/build/preprocessing/parse_vep_out.py -i $2 -o $2
