c1739f4af230de2bd9b4273a35a35edcfafa4165
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-06-08 17:42:47 +0200

    Add scripts to merge af structures

---
+++ build/datasets/af_merge.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Script to merge split pdbfiles from AlphaFold.
+
+**Author:**
+Natalia A. Szulc, nszulc@iimcb.gov.pl
+
+If you used this script, please check if our paper on DEGRONOPEDIA has been published (see section Citing at https://degronopedia.com/degronopedia/about), if not, please cite the preprint:
+
+Natalia A. Szulc, Filip Stefaniak, Małgorzata Piechota, Andrea Cappannini, Janusz M. Bujnicki, Wojciech Pokrzywa (2022).
+DEGRONOPEDIA - a web server for proteome-wide inspection of degrons
+doi: 10.1101/2022.05.19.492622
+
+Example: python3 merge_afold_files.py -u A2VEC9 -i example_uncompressed/ -v 4 -o example_uncompressed/out/ -g 0
+'''
+
+import argparse
+from Bio.PDB import *
+import gzip
+from os import listdir, sep
+from os.path import isfile, join
+import subprocess
+
+if __name__ == "__main__":
+
+    ###  ARGUMENTS PARSING  ###
+
+    parser = argparse.ArgumentParser(description = '''Tool merging split AlphaFold PDB files.''',
+                                     add_help = False,
+                                     formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+                                     conflict_handler = 'resolve')
+
+    required_arguments = parser.add_argument_group('Required arguments')
+    required_arguments.add_argument('-u', help='UniProt ID', required=True, metavar='UniProtID', default=argparse.SUPPRESS)
+    required_arguments.add_argument('-i', help='input path', required=True, metavar='Input path', default=argparse.SUPPRESS)
+    required_arguments.add_argument('-v', help='AlphaFold version', required=True, metavar='AlphaFold version', default=argparse.SUPPRESS)
+
+    optional_arguments = parser.add_argument_group('Optional arguments')
+    optional_arguments.add_argument('-o', help='output path', metavar='Output path')
+    optional_arguments.add_argument('-g', help='gzip file', type=int, default=1)
+    optional_arguments.add_argument('-h', action = 'help', help = 'show this help message and exit')
+    optional_arguments.add_argument('--help', '-h', action = 'help', help = 'show this help message and exit')
+
+    args = vars(parser.parse_args())
+
+    struct_name = args['u']
+    input_path = args['i']
+    afold_version = args['v']
+    output_path = args['o']
+    zip = args['g']
+
+    if input_path[-1] == sep:
+        input_path = input_path[:-1]
+
+    if output_path:
+        if output_path[-1] == sep:
+            output_path = output_path[:-1]
+        save_path = output_path
+    else:
+        save_path = input_path
+
+    ### FIND OUT HOW MANY PIECES ###
+
+    how_many_pieces = 0
+    onlyfiles = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+
+    for f in onlyfiles:
+        if struct_name in f and f[0] != '.': # do not include hidden files
+            how_many_pieces += 1
+
+    ### MERGING ###
+
+    Bio_parser = PDBParser()
+    c = 1
+    while c < how_many_pieces :
+
+        # Read reference structure
+        if c == 1:
+            if zip:  
+                with gzip.open(f'{input_path}/AF-{struct_name}-F{c}-model_v{afold_version}.pdb.gz', 'rt') as handle:
+                    structure_ref = Bio_parser.get_structure("ref", handle)
+            else:
+                with open(f'{input_path}/AF-{struct_name}-F{c}-model_v{afold_version}.pdb', 'r') as handle:
+                    structure_ref = Bio_parser.get_structure("ref", handle)
+        else:
+            structure_ref = Bio_parser.get_structure("ref", f"{save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb")
+        model_ref = structure_ref[0]
+
+        # Read structure to superimpose
+        if zip:  
+            with gzip.open(f'{input_path}/AF-{struct_name}-F{c+1}-model_v{afold_version}.pdb.gz', 'rt') as handle:
+                structure_to_superpose = Bio_parser.get_structure("ref", handle)
+        else:
+            with open(f'{input_path}/AF-{struct_name}-F{c+1}-model_v{afold_version}.pdb', 'r') as handle:
+                structure_to_superpose = Bio_parser.get_structure("ref", handle)
+        model_to_super = structure_to_superpose[0]
+
+        # Append atoms from the nine last residues except for the very last one (it is C-end, has one more atom more)
+        model_ref_atoms = []
+        for j in range(len(model_ref['A'])-9, len(model_ref['A'])):
+            for atom in model_ref['A'][j]:
+                model_ref_atoms.append(atom)
+
+        # Append atoms from the 1191-1999 residues which correspond the abovementioned residues from the reference
+        model_to_superpose_atoms = []
+        for j in range(1191, 1200):
+            for atom in model_to_super['A'][j]:
+                model_to_superpose_atoms.append(atom)
+
+        # Superimpose
+        sup = Superimposer()
+        sup.set_atoms(model_ref_atoms, model_to_superpose_atoms)
+
+        # Update coords of the residues from the structure to be superimposed
+        sup.apply(model_to_super.get_atoms())
+
+        # Delete last residue (C-end residue, with one atom more) from the reference structure
+        model_ref['A'].detach_child((' ', len(model_ref['A']), ' '))
+
+        # Delete first 1199 residues from the superimposed structure
+        for i in range(1, 1200):
+            model_to_super['A'].detach_child((' ', i, ' '))
+
+        # Renumber residues in the superimposed structure
+        # Do it twice as you cannot assign a number to a residue that another residue already has
+        tmp_resnums = [i+1 for i in range(len(model_to_super['A']))]
+
+        for i, residue in enumerate(model_to_super['A'].get_residues()):
+            res_id = list(residue.id)
+            res_id[1] = tmp_resnums[i]
+            residue.id = tuple(res_id)
+
+        new_resnums = [i+len(model_ref['A'])+1 for i in range(len(model_to_super['A']))]
+
+        for i, residue in enumerate(model_to_super['A'].get_residues()):
+            res_id = list(residue.id)
+            res_id[1] = new_resnums[i]
+            residue.id = tuple(res_id)
+
+        # Merge and save both structures however as two models
+        merged = Structure.Structure("master")
+        merged.add(model_ref)
+        model_to_super.id='B'
+        merged.add(model_to_super)
+
+        io = PDBIO()
+        io.set_structure(merged)
+        io.save(f"{save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb")
+
+        # Unify models
+        bashCommand1 = f"sed '/TER/d' {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb > {save_path}/tmp.pdb"
+        bashCommand2 = f"sed '/MODEL/d' {save_path}/tmp.pdb > {save_path}/tmp1.pdb"
+        bashCommand3 = f"sed '/ENDMDL/d' {save_path}/tmp1.pdb > {save_path}/tmp2.pdb"
+
+        subprocess.run(bashCommand1, check=True, text=True, shell=True)
+        subprocess.run(bashCommand2, check=True, text=True, shell=True)         
+        subprocess.run(bashCommand3, check=True, text=True, shell=True)
+
+        # Re-read the structure in Biopython and save
+        structure_ok = Bio_parser.get_structure("ok", f'{save_path}/tmp2.pdb')
+        io.set_structure(structure_ok)
+        io.save(f"{save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb")
+
+        c += 1
+
+    # Add MODEL 1 at the beggining of the file
+    #subprocess.run(f"gsed -i -e '1iMODEL        1                                                                  \' {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, text=True, shell=True)
+    subprocess.run(f"sed -i '1iMODEL        1                                                                  ' {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, shell=True)
+
+    # Provide proper file ending
+    #subprocess.run(f"gsed -i '$ d' {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, text=True, shell=True)
+    subprocess.run(f"sed -i '$ d' {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, shell=True)
+    subprocess.run(f"echo 'ENDMDL                                                                          ' >> {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, text=True, shell=True)
+    subprocess.run(f"echo 'END                                                                             ' >> {save_path}/AF-{struct_name}-FM-model_v{afold_version}.pdb", check=True, text=True, shell=True)
+
+
+    # Delete tmp files
+    subprocess.run(f"rm {save_path}/tmp.pdb {save_path}/tmp1.pdb {save_path}/tmp2.pdb", check=True, text=True, shell=True)
---
+++ build/datasets/af_merge_parser.py
@@ -0,0 +1,175 @@
+import os 
+import re
+import pandas as pd
+import subprocess
+import argparse
+from progressbar import progressbar
+import shutil
+import warnings
+from Bio.PDB import PDBParser
+
+"""
+In-house script that merge all fragmented structures in a given directory.
+To merge the pdb structures it uses the DEGRONOPEDIA script.
+
+Example usage:
+
+python3 af_merge_parser.py -i ./example_uncompresseds
+python3 af_merge_parser.py -i /workspace/projects/clustering_3d/clustering_3d/datasets_frag/pdb_structures
+python3 af_merge_parser.py -i ./example_uncompressed -o ./example_uncompressed/out/ 
+
+/workspace/projects/clustering_3d/clustering_3d/datasets_frag/pdb_structures
+"""
+
+# Add SEQREF record to pdb file
+
+def get_res_from_chain(pdb_path):
+    """
+    Get sequense of amino acid residues from the structure chain.
+    """
+    
+    # Load structure
+    parser = PDBParser()
+    structure = parser.get_structure("ID", pdb_path)
+    
+    # Get seq from chain
+    residues = []
+    chain = structure[0]["A"]
+    for residue in chain.get_residues():
+        residues.append(residue.resname)
+        
+    return residues
+
+
+def get_pdb_seqres_records(lst_res):
+    """
+    Construct the fixed-width records of a pdb file.
+    """
+    
+    records = []
+    num_residues = len(lst_res)
+    record_counter = 0
+    while record_counter * 13 < num_residues:
+        start_idx = record_counter * 13
+        end_idx = min(start_idx + 13, num_residues)
+        residue_subset = lst_res[start_idx:end_idx]
+        record = 'SEQRES {:>3} {} {:>4}  {:52}\n'.format(record_counter + 1, "A", num_residues, ' '.join(residue_subset))
+        records.append(record)
+        record_counter += 1
+        
+    return records
+
+
+def add_refseq_record_to_pdb(path_structure):
+    """
+    Add the SEQREF records to the pdb file.
+    """
+    
+    # Open the PDB file and get SEQRES insert index
+    with open(path_structure, 'r') as file:
+        pdb_lines = file.readlines()
+        insert_index = next(i for i, line in enumerate(pdb_lines) if line.startswith('MODEL'))
+
+    # Get seares records
+    residues = get_res_from_chain(path_structure)
+    seqres_records = get_pdb_seqres_records(residues)
+
+    # Insert the SEQRES records
+    pdb_lines[insert_index:insert_index] = seqres_records
+
+    # Save
+    with open(path_structure, 'w') as output_file:
+        output_file.truncate()
+        output_file.writelines(pdb_lines)
+
+
+# Other functions
+
+def get_list_fragmented_pdb(pdb_dir):
+    """
+    Given a directory including pdb files, return 
+    a list of tuples (Uniprot_ID, max AF_F).
+    """
+    
+    # List pdb files
+    list_pdb = os.listdir(pdb_dir)
+    list_pdb = [file for file in list_pdb if not file.startswith("tmp") and file.endswith(".pdb") or file.endswith(".pdb.gz")]
+    list_pdb = [(file.split("-")[1], re.sub(r"\D", "", file.split("-")[2])) for file in list_pdb if file.split("-")[2][-1] != "M"]
+    
+    # Get df with max fragment
+    df = pd.DataFrame(list_pdb, columns=["Uniprot_ID", "F"])
+    df["F"] = pd.to_numeric(df["F"])
+    df = df.groupby("Uniprot_ID").max()
+    
+    # Get fragmented structures as list of (Uniprot_ID AF_F) tuples
+    df = df[df["F"] > 1].reset_index()
+    
+    return list(df.to_records(index=False))
+
+
+def save_unprocessed_ids(uni_ids, filename):
+    
+    with open(filename, 'a') as file:
+        for id in uni_ids:
+            file.write(id + '\n')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--input_dir", help = "Path to directory including pdb structures", type=str, required=True)
+parser.add_argument("-o", "--output_dir", help = "Path to output directory", type=str)
+parser.add_argument("-v", "--af_version", help = "AlphaFold 2 version used to produced the pdb files", type=int, default=4)
+parser.add_argument("-g", "--zip", help = "1 if files is compressed with gzip, else 0", type=int, default=0)
+
+args = parser.parse_args()
+input_dir = args.input_dir
+output_dir = args.output_dir
+af_version = args.af_version
+zip = args.zip
+
+dir_path = os.path.abspath(os.path.dirname(__file__))
+path_script = f"{dir_path}/af_merge.py"
+
+if output_dir is None:
+    output_dir = input_dir
+    
+if zip:
+    zip_ext = ".gzip"
+else:
+    zip_ext = ""
+    
+# Create dir where to move original fragmented structures    
+path_original_frag = f"{output_dir}/fragmented_pdbs/"
+if not os.path.exists(path_original_frag):
+    os.makedirs(path_original_frag)
+    
+# Get list of fragmented Uniprot ID and max AF-F
+not_processed = []
+fragments = get_list_fragmented_pdb(input_dir)
+print("Merging structures..")
+for uni_id, max_f in progressbar(fragments):
+    
+    merge_command = [f"python3", path_script, "-u", uni_id, 
+                     "-i", input_dir, "-v", f"{af_version}", "-o", output_dir, "-g", f"{zip}"]
+    processed = 0
+    
+    try:
+        run = subprocess.run(merge_command, check=True)
+        processed = 1
+    except:
+        warnings.warn(f"WARNING........... could not process {uni_id} ({max_f} fragments)")
+        not_processed.append(uni_id)
+        os.remove(f"{output_dir}/AF-{uni_id}-FM-model_v{af_version}.pdb")
+    
+    # Move the original fragmented structures
+    for f in range(1, max_f+1):
+        file = f"AF-{uni_id}-F{f}-model_v{af_version}.pdb{zip_ext}"
+        shutil.move(f"{input_dir}/{file}", path_original_frag)
+        
+    # Rename merged structure and add refseq records to pdb
+    if processed:
+        tmp_name = f"{output_dir}/AF-{uni_id}-FM-model_v{af_version}.pdb"
+        name = f"{output_dir}/AF-{uni_id}-F{max_f}M-model_v{af_version}.pdb"
+        os.rename(tmp_name, name)
+        add_refseq_record_to_pdb(name)
+
+save_unprocessed_ids(not_processed, f"{output_dir}/fragmented_pdbs/not_processed.txt")
