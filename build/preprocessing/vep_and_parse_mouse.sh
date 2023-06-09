8d84439febbc0bbc92b9efb375caec4eb55ca135
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-06-09 16:56:47 +0200

    Debug vep parser

--- build/preprocessing/vep_and_parse_mouse.sh
+++ build/preprocessing/vep_and_parse_mouse.sh
@@ -26,12 +26,6 @@ if [[ -z "$1" || -z "$2" ]]; then
     exit 0
 fi
 
-# # Sort file by chromosome and position
-# echo "Sorting by chr and pos.."
-# temp_file=$(mktemp) 
-# sort -k1,1 -k2,2n "$1" > "$temp_file"
-# mv "$temp_file" "$1"
-
 # Run VEP
 echo "Running VEP.."
 singularity exec /workspace/datasets/vep/mus_musculus/vep102.sif vep --dir /workspace/datasets/vep/ -i $1 --offline --cache -o $2 --species mus_musculus --assembly GRCm38 --fork 8 --symbol --protein --tab --canonical --pick
@@ -39,10 +33,11 @@ singularity exec /workspace/datasets/vep/mus_musculus/vep102.sif vep --dir /work
 rm $2"_summary.html"
 
 # Remove comments in the header
-temp_file=$(mktemp)  
-cat "$2" | grep -v "^##" > "$temp_file"
-mv "$temp_file" "$2"
+echo $2".temp"
+cat $2 | grep -v "^##" > $2".temp"
+# mv "$temp_file" "$2"
 
 # Parse output
 echo "Parsing output.."
-python3 /workspace/projects/clustering_3d/clustering_3d/build/preprocessing/parse_vep_out.py -i $2 -o $2
+python3 /workspace/projects/clustering_3d/clustering_3d/build/preprocessing/parse_vep_out.py -i $2".temp" -o $2
+rm $2
