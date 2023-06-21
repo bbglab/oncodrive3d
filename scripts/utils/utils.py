fd1888e34f3ecfedfd80a3863229a4d3613535a8
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-06-21 16:07:10 +0200

    Fix MAF parser including any consequence with a missense mutation

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -16,7 +16,7 @@ def parse_maf_input(maf_input_path, keep_samples_id=False):
     maf = pd.read_csv(maf_input_path, sep="\t", dtype={'Chromosome': str})
 
     # Select only missense mutation and extract Gene name and mut
-    maf = maf[maf['Variant_Classification'].str.contains('missense_variant')]
+    maf = maf[maf['Variant_Classification'].str.contains('Missense_Mutation')]
     maf["Pos"] = maf.loc[:, "HGVSp_Short"].apply(lambda x: int(re.sub("\\D", "", (x[2:]))))
     maf["WT"] = maf["HGVSp_Short"].apply(lambda x: re.findall("\\D", x[2:])[0])
     maf["Mut"] = maf["HGVSp_Short"].apply(lambda x: re.findall("\\D", x[2:])[1])
