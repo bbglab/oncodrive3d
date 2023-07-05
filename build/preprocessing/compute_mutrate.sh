489ce439f0a6df789e3208673cb0333763b2589d
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-07-05 18:09:27 +0200

    Fix output filename of each cohort

--- build/preprocessing/compute_mutrate.sh
+++ build/preprocessing/compute_mutrate.sh
@@ -3,7 +3,7 @@
 ###########################
 # Example
 # ./bgsign.sh <input_dir_to_maf_files> <output_mut_profile_dir> <regions_file> <genome> <cores>
-# ./bgsign.sh toy_maf/ mut_profile/ hg38_wg_regions.tsv hg38 10
+# ./compute_mutrate.sh /workspace/projects/clustering_3d/evaluation/datasets/input/maf/ /workspace/projects/clustering_3d/evaluation/datasets/input/mut_profile_bgsign/mut_profile /workspace/projects/clustering_3d/evaluation/datasets/input/mut_profile_bgsign/hg38_wg_regions.tsv hg38 10
 
 # To get region file
 # python3 /workspace/projects/intogen_plus/fixdatasets-20230223/intogen-plus/build/datasets/regions/create_wg_regions.py hg38 3 > hg38_wg_regions.tsv
@@ -29,7 +29,8 @@ for file in "$input_dir"/*; do
 
     # Get the filename without the directory path
     filename=$(basename "$file")
-    
+    filename="${filename%%.in.maf}"
+
     # Get count of mutation per context and normalize by trinucleotide bias (mut profile)
     bgsignature normalize -m $file -r $regions --normalize $output_dir/wg.count.json -s 3 -g $genome --collapse --cores 4 -o $output_dir/${filename}.mutrate.json
 
