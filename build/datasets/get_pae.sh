a7e3eacfe063bba6692af43774ff6d8dc7f7dc60
St3451 <stefano.pellegrini@irbbarcelona.org>
2023-07-12 17:54:14 +0200

    Add scripts to download and parse PAE

---
+++ build/datasets/get_pae.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Help message
+function show_help() {
+    echo "Usage: ./get_pae.sh [arg1] [arg2]"
+    echo ""
+    echo "Args:"
+    echo "  arg1 : Specify the path of the sequence df (seq_for_mut_prob.csv) storing the Uniprot_IDs."
+    echo "  arg2 : Specify the directory where to save the predicted aligned errors."
+    echo ""
+    echo "Example:"
+    echo "./get_pae.sh /workspace/projects/clustering_3d/clustering_3d/datasets_frag/seq_for_mut_prob.csv /workspace/projects/clustering_3d/clustering_3d/datasets_frag/pae"
+    echo ""
+}
+
+filename="$1"
+output_dir="$2"
+
+# Check if the required arguments are provided
+if [ -z "$filename" ] || [ -z "$output_dir" ]; then
+    show_help
+    exit 0
+fi
+
+# Create the output directory if it doesn't exist
+if [ ! -d "$output_dir" ]; then
+    mkdir "$output_dir"
+fi
+
+# Read the seq file and iterate through the (non-fragmented) Uniprot_ID
+while IFS=',' read -ra cols
+do
+
+    # Check if the third element is equal to 1
+    if [[ ${cols[2]} == 1 ]]; then
+
+        # Download PAE from AF databasae
+        wget -O $output_dir/${cols[1]}-F1-predicted_aligned_error.json https://alphafold.ebi.ac.uk/files/AF-${cols[1]}-F1-predicted_aligned_error_v4.json
+
+    fi
+
+    # Check that the file is not truncated
+    if ! tail -n 1 $output_dir/${cols[1]}-F1-predicted_aligned_error.json | grep -q "}]" ; then
+        echo The ${cols[1]} is incomplete.. re-downloading..
+        wget -O $output_dir/${cols[1]}-F1-predicted_aligned_error.json https://alphafold.ebi.ac.uk/files/AF-${cols[1]}-F1-predicted_aligned_error_v4.json
+
+    fi
+
+done < "$filename"
---
+++ build/datasets/parse_pae.py
@@ -0,0 +1,67 @@
+import numpy as np
+import os
+import json
+import argparse
+import re
+from progressbar import progressbar
+
+
+###################################### EXAMPLE USAGE ################################################
+
+# python3 parse_pae.py -i /workspace/projects/clustering_3d/clustering_3d/datasets_frag/pae
+
+#####################################################################################################
+
+
+def init_parser():
+    """
+    Initialize parser for the main function.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="Path to directory including the json files (predicted aligned error)", type=str, required=True) 
+    parser.add_argument("-o", "--output", help="Path to the output dir", type=str) 
+    return parser.parse_args()
+
+
+def get_pae_path_list_from_dir(path_dir):
+    """
+    Takes as input the path of a directory and it 
+    outputs a list of paths of the contained PAE files.
+    """
+
+    pae_files = os.listdir(path_dir)
+    pae_files = [f"{path_dir}/{f}" for f in pae_files if re.search('-predicted_aligned_error.json$', f) is not None]
+    
+    return pae_files
+
+
+def json_to_npy(path):
+    
+    print(path)
+    with open(path) as f:
+        pae = json.load(f)
+        
+    return np.array(pae[0]['predicted_aligned_error'])
+
+
+def main():
+    
+    args = init_parser()
+    input = args.input
+    output = args.output
+    
+    if output is None:
+        output = input
+        
+    # Create necessary folder
+    if not os.path.exists(output):
+        os.makedirs(output)
+        
+    path_files = get_pae_path_list_from_dir(input)
+
+    for path in progressbar(path_files[16525:]):
+        np.save(path.replace(".json", ".npy"), json_to_npy(path))
+
+if __name__ == "__main__":
+    main()
