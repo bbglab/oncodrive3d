26f5f1f7c8aa75c05d8b49d0fef47f6170759d8d
Federica Brando <federica.brando@irbbarcelona.org>
2023-08-31 16:04:31 +0200

    fix: ability to parse gzip files as well as uncompress

--- scripts/datasets/build_datasets.py
+++ scripts/datasets/build_datasets.py
@@ -25,19 +25,20 @@ The build is a pipeline that perform the following tasks:
 #############
 
 
-import daiquiri
+import os
 import subprocess
 
-import os
+import daiquiri
 
 from scripts import __logger_name__
-from scripts.globals import clean_dir
 from scripts.datasets.af_merge import merge_af_fragments
+from scripts.datasets.get_pae import get_pae
+from scripts.datasets.get_structures import get_structures
 from scripts.datasets.model_confidence import get_confidence
-from scripts.datasets.seq_for_mut_prob import get_seq_df
 from scripts.datasets.parse_pae import parse_pae
 from scripts.datasets.prob_contact_maps import get_prob_cmaps_mp
-from scripts.datasets.get_structures import get_structures
+from scripts.datasets.seq_for_mut_prob import get_seq_df
+from scripts.globals import clean_dir
 
 logger = daiquiri.getLogger(__logger_name__ + ".build")
 
@@ -57,15 +58,17 @@ def build(output_datasets,
 
     # Download PDB structures
     logger.info("Downloading AlphaFold (AF) predicted structures..")
-    get_structures(f"{output_datasets}/pdb_structures",
-                   organism,
-                   str(af_version), num_cores)
+    get_structures(path=os.path.join(output_datasets,"pdb_structures"),
+                   species=organism,
+                   af_version=str(af_version), 
+                   threads=num_cores)
 
     logger.info("Download of structures completed")
 
     # Merge fragmented structures
     logger.info("Merging fragmented structures..")
-    merge_af_fragments(input_dir=f"{output_datasets}/pdb_structures", gzip=True)
+    merge_af_fragments(input_dir=os.path.join(output_datasets,"pdb_structures"), 
+                       gzip=True)
     logger.info("Merge of structures completed")
 
     # Get model confidence
@@ -77,8 +80,8 @@ def build(output_datasets,
 
     # Create df including genes and proteins sequences & Hugo to Uniprot_ID mapping
     logger.info("Generating dataframe for genes and proteins sequences..")
-    get_seq_df(input_dir=f"{output_datasets}/pdb_structures",
-               output_seq_df=f"{output_datasets}/seq_for_mut_prob.csv",
+    get_seq_df(input_dir=os.path.join(output_datasets,"pdb_structures"),
+               output_seq_df=os.path.join(output_datasets, "seq_for_mut_prob.csv"),
                uniprot_to_gene_dict=uniprot_to_hugo,
                organism=organism)
     logger.info("Generation of sequences dataframe completed!")
@@ -99,9 +102,9 @@ def build(output_datasets,
 
     # Get pCAMPs
     logger.info("Generating contact probability maps (pCMAPs)..")
-    get_prob_cmaps_mp(input_pdb=f"{output_datasets}/pdb_structures",
-                      input_pae=f"{output_datasets}/pae",
-                      output=f"{output_datasets}/prob_cmaps",
+    get_prob_cmaps_mp(input_pdb=os.path.join(output_datasets, "pdb_structures"),
+                      input_pae=os.path.join(output_datasets, "pae"),
+                      output=os.path.join(output_datasets,"prob_cmaps"),
                       distance=10,
                       num_cores=num_cores)
     logger.info("Generation pCMAPs completed")
