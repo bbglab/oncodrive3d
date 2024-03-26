7120ee6418eca380c14782a2941b3dc566396c77
St3451 <stefano.pellegrini@irbbarcelona.org>
2024-03-26 18:32:11 +0100

    Comment out build_annotations (pushed by mistake)

--- scripts/plotting/build_annotations.py
+++ scripts/plotting/build_annotations.py
@@ -34,45 +34,45 @@ def get_annotations(data_dir,
     Main function to build annotations to generate annotated plots.
     """
 
-    # # Empty directory and load sequence df
-    # clean_annot_dir(output_dir, 'd')
+    # Empty directory and load sequence df
+    clean_annot_dir(output_dir, 'd')
     seq_df = pd.read_csv(os.path.join(data_dir, "seq_for_mut_prob.tsv"), sep="\t")    
 
-    # # Download DDG
-    # species = get_species(organism)
-    # logger.info(f"Downloading stability change...")
-    # if species == "Homo sapiens":
-    #     ddg_output = os.path.join(output_dir, "stability_change")
-    #     if not os.path.isdir(ddg_output):
-    #         os.makedirs(ddg_output)
-    #         logger.debug(f'mkdir {ddg_output}')
-    #     file_path = download_stability_change(ddg_output, cores)
-    #     logger.info(f"Download completed!")
+    # Download DDG
+    species = get_species(organism)
+    logger.info(f"Downloading stability change...")
+    if species == "Homo sapiens":
+        ddg_output = os.path.join(output_dir, "stability_change")
+        if not os.path.isdir(ddg_output):
+            os.makedirs(ddg_output)
+            logger.debug(f'mkdir {ddg_output}')
+        file_path = download_stability_change(ddg_output, cores)
+        logger.info(f"Download completed!")
         
-    #     ## TODO: Optimize DDG parsing 
-    #     ##       - only one protein is allocated to one process every time
-    #     ##       - a list of proteins should be allocated instead
+        ## TODO: Optimize DDG parsing 
+        ##       - only one protein is allocated to one process every time
+        ##       - a list of proteins should be allocated instead
         
-    #     # Parsing DDG
-    #     logger.info(f"Parsing stability change...")
-    #     parse_ddg_rasp(file_path, ddg_output, cores)
-    #     logger.info(f"Parsing completed!")
-    # else:
-    #     logger.warning(f"Currently, stability change annotation is not available for {species} but only for Homo sapiens: Skipping...")
+        # Parsing DDG
+        logger.info(f"Parsing stability change...")
+        parse_ddg_rasp(file_path, ddg_output, cores)
+        logger.info(f"Parsing completed!")
+    else:
+        logger.warning(f"Currently, stability change annotation is not available for {species} but only for Homo sapiens: Skipping...")
     
-    # ## TODO: Enable multiprocessing for PDB_Tool
-    # ## TODO: Evaluate the possibility of installing PDB_Tool instead of using container
+    ## TODO: Enable multiprocessing for PDB_Tool
+    ## TODO: Evaluate the possibility of installing PDB_Tool instead of using container
     
-    # # Run PDB_Tool
-    # logger.info(f"Extracting pACC and 2° structure...")
-    # path_pdb_structure = os.path.join(data_dir, "pdb_structures")
-    # pdb_tool_output = run_pdb_tool(input_dir=path_pdb_structure, output_dir=output_dir)
-    # logger.info(f"Extraction completed!")
+    # Run PDB_Tool
+    logger.info(f"Extracting pACC and 2° structure...")
+    path_pdb_structure = os.path.join(data_dir, "pdb_structures")
+    pdb_tool_output = run_pdb_tool(input_dir=path_pdb_structure, output_dir=output_dir)
+    logger.info(f"Extraction completed!")
     
-    # # Parse PDB_Tool
-    # logger.info(f"Parsing pACC and 2° structures...")
-    # parse_pdb_tool(input_dir=pdb_tool_output, output_dir=output_dir)
-    # logger.info(f"Parsing completed!")
+    # Parse PDB_Tool
+    logger.info(f"Parsing pACC and 2° structures...")
+    parse_pdb_tool(input_dir=pdb_tool_output, output_dir=output_dir)
+    logger.info(f"Parsing completed!")
     
     # Get Pfam annotations
     logger.info(f"Downloading and parsing Pfam...")
