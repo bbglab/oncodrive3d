fe001a83201b47a6390b23af8a9eebac37e34071
St3451 <stefano.pellegrini@irbbarcelona.org>
2024-11-18 15:22:54 +0100

    Debugging mode from pfam annotations

--- scripts/plotting/build_annotations.py
+++ scripts/plotting/build_annotations.py
@@ -36,54 +36,55 @@ def get_annotations(data_dir,
     Main function to build annotations to generate annotated plots.
     """
 
-    # Empty directory and load sequence df
-    clean_annot_dir(output_dir, 'd')
+    # # Empty directory and load sequence df
+    # clean_annot_dir(output_dir, 'd')
 
-    # Get ddG
+    # # Get ddG
     species = get_species(organism)
-    logger.info(f"Obtaining stability change..")
-    if species == "Homo sapiens" or species == "Mus musculus":
-        ddg_output = os.path.join(output_dir, "stability_change")
-        os.makedirs(ddg_output, exist_ok=True)
-        if ddg_dir is not None:
-            # Copy ddG from path
-            temp_ddg_path = os.path.join(output_dir, "stability_change_temp")
-            copy_dir(source_dir=ddg_dir, destination_dir=temp_ddg_path)
-        else:    
-            # Download ddG
-            temp_ddg_path = download_stability_change(ddg_output, cores)
-        logger.info(f"Completed!")
+    # logger.info(f"Obtaining stability change..")
+    # if species == "Homo sapiens" or species == "Mus musculus":
+    #     ddg_output = os.path.join(output_dir, "stability_change")
+    #     os.makedirs(ddg_output, exist_ok=True)
+    #     if ddg_dir is not None:
+    #         # Copy ddG from path
+    #         temp_ddg_path = os.path.join(output_dir, "stability_change_temp")
+    #         copy_dir(source_dir=ddg_dir, destination_dir=temp_ddg_path)
+    #     else:    
+    #         # Download ddG
+    #         temp_ddg_path = download_stability_change(ddg_output, cores)
+    #     logger.info(f"Completed!")
         
-        ## TODO: Optimize DDG parsing 
-        ##       - only one protein is allocated to one process every time
-        ##       - a list of proteins should be allocated instead
+    #     ## TODO: Optimize DDG parsing 
+    #     ##       - only one protein is allocated to one process every time
+    #     ##       - a list of proteins should be allocated instead
         
-        # Parsing DDG
-        logger.info(f"Parsing stability change..")
-        parse_ddg_rasp(temp_ddg_path, ddg_output, cores)
-        logger.info(f"Parsing completed!")
-    else:
-        logger.warning(f"Currently, stability change annotation is not available for {species} but only for Homo sapiens: Skipping...")
+    #     # Parsing DDG
+    #     logger.info(f"Parsing stability change..")
+    #     parse_ddg_rasp(temp_ddg_path, ddg_output, cores)
+    #     logger.info(f"Parsing completed!")
+    # else:
+    #     logger.warning(f"Currently, stability change annotation is not available for {species} but only for Homo sapiens: Skipping...")
     
-    ## TODO: Enable multiprocessing for PDB_Tool
-    ## TODO: Evaluate the possibility of installing PDB_Tool instead of using container
+    # ## TODO: Enable multiprocessing for PDB_Tool
+    # ## TODO: Evaluate the possibility of installing PDB_Tool instead of using container
     
-    # Run PDB_Tool
-    logger.info(f"Extracting pACC and 2° structure..")
-    path_pdb_structure = os.path.join(data_dir, "pdb_structures")
-    pdb_tool_output = run_pdb_tool(input_dir=path_pdb_structure, output_dir=output_dir)
-    logger.info(f"Extraction completed!")
+    # # Run PDB_Tool
+    # logger.info(f"Extracting pACC and 2° structure..")
+    # path_pdb_structure = os.path.join(data_dir, "pdb_structures")
+    # pdb_tool_output = run_pdb_tool(input_dir=path_pdb_structure, output_dir=output_dir)
+    # logger.info(f"Extraction completed!")
     
-    # Parse PDB_Tool
-    logger.info(f"Parsing pACC and 2° structures..")
-    parse_pdb_tool(input_dir=pdb_tool_output, output_dir=output_dir)
-    logger.info(f"Parsing completed!")
+    # # Parse PDB_Tool
+    # logger.info(f"Parsing pACC and 2° structures..")
+    # parse_pdb_tool(input_dir=pdb_tool_output, output_dir=output_dir)
+    # logger.info(f"Parsing completed!")
     
     # Get Pfam annotations
     logger.info(f"Downloading and parsing Pfam..")
     seq_df = pd.read_csv(os.path.join(data_dir, "seq_for_mut_prob.tsv"), sep="\t")    
     pfam_df = get_pfam(seq_df = seq_df, 
-                       output_tsv = os.path.join(output_dir, "pfam.tsv"))
+                       output_tsv = os.path.join(output_dir, "pfam.tsv"),
+                       organism=organism)
     logger.info(f"Completed!")
     
     # Get Uniprot features
